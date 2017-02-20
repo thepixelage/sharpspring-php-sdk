7bf064264d2026be89f9b2fa0f9dec6fad29fc11
Ron Lim <ron.lim@thepixelage.com>
2017-02-20 19:58:38 +0800

    Add dotenv

--- src/SharpSpring.php
+++ src/SharpSpring.php
@@ -2,14 +2,24 @@
 
 namespace thepixelage\SharpSpring;
 
+use Dotenv\Dotenv;
+
 class SharpSpring
 {
     private $accountID;
     private $secretKey;
 
-    public function __construct($accountID, $secretKey)
+    public function __construct()
     {
-        $this->accountID = $accountID;
-        $this->secretKey = $secretKey;
+        $dotenv = new Dotenv(__DIR__ . '/../');
+        $dotenv->load();
+
+        $this->accountID = getenv('SS_ACCOUNT_ID');
+        $this->secretKey = getenv('SS_SECRET_KEY');
+    }
+
+    public function getSecretKey()
+    {
+        return $this->secretKey;
     }
 }
